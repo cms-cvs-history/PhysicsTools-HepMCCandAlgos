1739271849ef12a5dd4b5ffad564ac553934af49
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2008-12-22 19:44:25 +0000

    Adding flavor history filter correctly to branch B2_2_X

--- python/flavorHistoryFilter_cfi.py
+++ python/flavorHistoryFilter_cfi.py
@@ -1,51 +1,155 @@
 import FWCore.ParameterSet.Config as cms
 
-wbb_me_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+
+dr0 = 0.0
+dr1 = 0.5
+dr2 = 9999.0
+
+# w+bb
+# matrix element
+# widely separated
+wbbMEFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
                                         src = cms.InputTag("bFlavorHistoryProducer", "bPartonFlavorHistory"),
-                                        jets = cms.InputTag("iterativeCone5GenJets"),
-                                        type = cms.int32(3), # only look at matrix element products
-                                        minPt = cms.double(20.0),
-                                        minDR = cms.double(0.5),
                                         scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(5),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(2,3),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr1),
+                                        maxDR = cms.double(dr2),
                                         verbose = cms.bool(False)
                                         )
 
-wbb_gs_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+# w+bb
+# matrix element
+# colinear (going to junk pile)
+wbbMEComplimentFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
                                         src = cms.InputTag("bFlavorHistoryProducer", "bPartonFlavorHistory"),
-                                        jets = cms.InputTag("iterativeCone5GenJets"),
-                                        type = cms.int32(1), # only look at gluon splitting products
-                                        minPt = cms.double(20.0),
-                                        minDR = cms.double(0.5),
                                         scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(5),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(2,3),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr0),
+                                        maxDR = cms.double(dr1),
                                         verbose = cms.bool(False)
                                         )
 
-wcc_me_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
-                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
-                                        jets = cms.InputTag("iterativeCone5GenJets"),
-                                        type = cms.int32(3), # only look at matrix element products
-                                        minPt = cms.double(20.0),
-                                        minDR = cms.double(0.5),
+# w+bb
+# gluon splitting
+# colinear
+wbbGSFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("bFlavorHistoryProducer", "bPartonFlavorHistory"),
                                         scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(5),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(1),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr0),
+                                        maxDR = cms.double(dr1),
                                         verbose = cms.bool(False)
                                         )
 
-wcc_gs_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
-                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
-                                        jets = cms.InputTag("iterativeCone5GenJets"),
-                                        type = cms.int32(1), # only look at gluon splitting products
-                                        minPt = cms.double(20.0),
-                                        minDR = cms.double(0.5),
+# w+bb
+# gluon splitting
+# widely separated (going to junk pile)
+wbbGSComplimentFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("bFlavorHistoryProducer", "bPartonFlavorHistory"),
                                         scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(5),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(1),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr1),
+                                        maxDR = cms.double(dr2),
                                         verbose = cms.bool(False)
                                         )
 
-wc_fe_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
-                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
-                                        jets = cms.InputTag("iterativeCone5GenJets"),
-                                        type = cms.int32(2), # only look at flavor excitation products
-                                        minPt = cms.double(20.0),
-                                        minDR = cms.double(0.5),
+
+# w+b
+# flavor excitation
+wbFEFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("bFlavorHistoryProducer", "bPartonFlavorHistory"),
                                         scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(5),
+                                        noutput = cms.int32(1),
+                                        flavorSource = cms.vint32(2,3),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr0),
+                                        maxDR = cms.double(dr0),
+                                        verbose = cms.bool(False)
+                                        )
+
+# w+cc
+# matrix element
+# widely separated
+wccMEFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(4),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(2,3),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr1),
+                                        maxDR = cms.double(dr2),
+                                        verbose = cms.bool(False)
+                                        )
+
+# w+cc
+# matrix element
+# colinear (going to junk pile)
+wccMEComplimentFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(4),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(2,3),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr0),
+                                        maxDR = cms.double(dr1),
+                                        verbose = cms.bool(False)
+                                        )
+
+
+# w+cc
+# gluon splitting
+# colinear
+wccGSFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(4),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(1),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr0),
+                                        maxDR = cms.double(dr1),
+                                        verbose = cms.bool(False)
+                                        )
+
+# w+cc
+# gluon splitting
+# widely separated (going to junk pile)
+wccGSComplimentFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(4),
+                                        noutput = cms.int32(2),
+                                        flavorSource = cms.vint32(1),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr1),
+                                        maxDR = cms.double(999.0),
+                                        verbose = cms.bool(False)
+                                        )
+# w+c
+# flavor excitation
+wcFEFlavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        scheme = cms.string("deltaR"),
+                                        flavor = cms.int32(4),
+                                        noutput = cms.int32(1),
+                                        flavorSource = cms.vint32(2,3),
+                                        minPt = cms.double(-1.0),
+                                        minDR = cms.double(dr0),
+                                        maxDR = cms.double(dr0),
                                         verbose = cms.bool(False)
                                         )
